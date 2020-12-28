39f863758e17d1871d201e0c557de6728745f4a6
Umut Arda Kapan <umutardakapan@gmail.com>
2020-12-29 02:13:14 +0300

    Add files via upload

---
+++ Assets/scr2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr2 : MonoBehaviour
+{
+    public GameObject esc3;
+
+    public bool cs = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cs)
+        {
+            esc3.SetActive(true);
+        }
+    }
+}
